e0f755a4042f82921c873c5f065e980c70fbc6d7
JoshPineda <jpinedainbox@gmail.com>
2019-02-07 00:15:17 -0500

    Adding folders/formatting/random buttons

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,16 +3,33 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 
 
 public class Main extends Application {
+	private static GridPane makeGridPane() {
+		Button addB = new Button("Add");
+		Button delB = new Button("Delete");
+		Button editB = new Button("Edit");
+		
+		
+		GridPane gridpane = new GridPane();
+		gridpane.add(addB, 0, 0);
+		gridpane.add(delB, 0, 1);
+		gridpane.add(editB, 0, 2);
+		return gridpane;
+	}
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			//test
-			BorderPane root = new BorderPane();
+			GridPane root = makeGridPane();
 			Scene scene = new Scene(root,400,400);
+			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
